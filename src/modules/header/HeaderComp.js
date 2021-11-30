94699a88631ec7e42091c9bc07ae8c1b8935a2a7
iamashutoshp <44366348+iamashutoshp@users.noreply.github.com>
2021-11-30 18:54:41 +0530

    feat/headerComp V0.0011 : account button added on header

--- src/modules/header/HeaderComp.js
+++ src/modules/header/HeaderComp.js
@@ -1,47 +1,67 @@
 import React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Toolbar from "@mui/material/Toolbar";
-import { styled} from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 import MuiAppBar from "@mui/material/AppBar";
 import IconButton from "@mui/material/IconButton";
+import Box from "@mui/material/Box";
+import DashboardSharpIcon from "@mui/icons-material/DashboardSharp";
+import Grid from "@mui/material/Grid";
+import AccountCircleSharpIcon from "@mui/icons-material/AccountCircleSharp";
 import "./header.css";
-import DashboardSharpIcon from '@mui/icons-material/DashboardSharp';
 
 const drawerWidth = 240;
 
 const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== "open",
-  })(({ theme, open }) => ({
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
     transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
     }),
-    ...(open && {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: `${drawerWidth}px`,
-      transition: theme.transitions.create(["margin", "width"], {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    }),
-  }));
+  }),
+}));
 
+const toolbarStyle = {
+  minHeight: "20px",
+};
 export default function HeaderComp(props) {
   return (
-    <div>
+    <Box sx={{ flexGrow: 1 }}>
       <CssBaseline />
-      <AppBar position="fixed" open={props.open} enableColorOnDark className="appbar">
-        <Toolbar>
-          <IconButton
-            aria-label="open drawer"
-            onClick={props.handleDrawerOpen}
-            edge="start"
-            sx={{ mr: 1, ...(props.open && { display: "none" }) }}
-          >
-            <DashboardSharpIcon color="secondary"/>
-          </IconButton>
-        </Toolbar>
+      <AppBar
+        position="fixed"
+        open={props.open}
+        enableColorOnDark
+        className="appbar"
+      >
+        <Grid container spacing={3}>
+          <Grid item xs={11}>
+            <Toolbar style={toolbarStyle}>
+              <IconButton
+                aria-label="open drawer"
+                onClick={props.handleDrawerOpen}
+                edge="start"
+                sx={{ mr: 1, ...(props.open && { display: "none" }) }}
+              >
+                <DashboardSharpIcon color="secondary" />
+              </IconButton>
+            </Toolbar>
+          </Grid>
+          <Grid item xs={1}>
+            <IconButton style={{ margin: "2px" }}>
+              <AccountCircleSharpIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
       </AppBar>
-    </div>
+    </Box>
   );
 }
